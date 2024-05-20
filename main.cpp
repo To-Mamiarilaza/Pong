123b2a23f347f43727d892c097a2017b7bd626b6
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-05-20 21:12:28 +0300

    Completed player movement

--- main.cpp
+++ main.cpp
@@ -16,6 +16,29 @@ class Ball {
     void Update() {
         x += speedX;
         y += speedY;
+
+        if (y + radius >= GetScreenHeight() || y <= 0) speedY *= -1;
+        if (x + radius >= GetScreenWidth() || x <= 0) speedX *= -1;
+        
+    }
+};
+
+class Paddle {
+    public:
+    float x, y;
+    float width, height;
+    int speed;
+
+    void Draw() {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void Update() {
+        if (IsKeyDown(KEY_UP)) y += -speed;
+        if (IsKeyDown(KEY_DOWN)) y += speed;
+
+        if (y <= 0) y = 0;
+        if (y + height >= GetScreenHeight()) y = GetScreenHeight() - height;
     }
 };
 
@@ -37,19 +60,30 @@ int main()
     ball.speedX = 7;
     ball.speedY = 7;
 
+    // Init the player
+    Paddle player;
+    player.width = 25;
+    player.height = 120;
+    player.x = screenWidth - player.width - 10;
+    player.y = screenHeight / 2 - player.height / 2;
+    player.speed = 6;
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
         // Updating
         ball.Update();
+        player.Update();
 
         // Drawing
         ClearBackground(BLACK);
         DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
+
         ball.Draw();
+        player.Draw();
         DrawRectangle(10, screenHeight / 2 - 60, 25, 120, WHITE);
-        DrawRectangle(screenWidth - 35, screenHeight / 2 - 60, 25, 120, WHITE);
+        
 
         EndDrawing();
     }
