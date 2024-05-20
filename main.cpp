15c2222174dee0772f6646fd30fefa242f06984b
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-05-20 21:37:12 +0300

    Complete collision

--- main.cpp
+++ main.cpp
@@ -118,6 +118,18 @@ int main()
         player.Update();
         cpu.Update(ball.y);
 
+        // Check for collision
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+        {
+            ball.speedX *= -1;
+        }
+
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+        {
+            ball.speedX *= -1;
+        }
+        
+
         // Drawing
         ClearBackground(BLACK);
         DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
